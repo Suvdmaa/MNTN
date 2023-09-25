868c9673623d40418be9a83b2a68a5dc91bc7c74
Suvdmaa <e.suvd1230@gmail.com>
2023-09-25 17:16:28 +0800

    MNTN

--- assets/js/common.js
+++ assets/js/common.js
@@ -3,3 +3,15 @@ $(".quick a").on("click", function () {
   $(this).addClass("active");
 });
 AOS.init();
+
+const toggleBtn = document.querySelector('.toogle-btn');
+const sidebar = document.querySelector('.sidebar');
+const toggleMenu = document.getElementById('toggle-menu');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('show-sidebar');
+});
+
+toggleMenu.addEventListener('change', () => {
+  sidebar.classList.toggle('show-sidebar', toggleMenu.checked);
+});
--- assets/js/contents.js
+++ assets/js/contents.js
@@ -9,3 +9,36 @@ scrollDown.addEventListener("click", ()=>{
         behavior: "smooth"
     })
 })
+
+window.addEventListener("scroll", function () {
+    let scrollPosition = window.scrollY;
+
+    let sections = [
+      { id: "header", offset: document.getElementById("header").offsetTop },
+      { id: "block1", offset: document.getElementById("block1").offsetTop },
+      { id: "block2", offset: document.getElementById("block2").offsetTop },
+      { id: "block3", offset: document.getElementById("block3").offsetTop },
+    ];
+
+    for (let i = 0; i < sections.length; i++) {
+      let section = sections[i];
+
+      if (
+        scrollPosition >= section.offset &&
+        (i === sections.length - 1 ||
+          scrollPosition < sections[i + 1].offset)
+      ) {
+        let links = document.querySelectorAll(".quick a");
+        links.forEach(function (link) {
+          link.classList.remove("active");
+        });
+
+        let activeLink = document.querySelector(
+          '.quick a[href="#' + section.id + '"]'
+        );
+        if (activeLink) {
+          activeLink.classList.add("active");
+        }
+      }
+    }
+  });
